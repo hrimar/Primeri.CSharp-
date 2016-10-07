7cca9ff42d6ae2c2d1d152d2615efa6f118fe38d
Hristo <hhristoff@yahoo.com>
2016-10-07 11:29:08 +0300

    Модул 02, Видео 07 - Работа с масиви

---
+++ Modul02/MyTries02-06/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyTries0206
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+		
+			String a = "Hello";
+			String b = "Word";
+			Object c = a + " + " + b;
+
+
+			Console.WriteLine (a + " + " + b);
+		}
+	}
+}
