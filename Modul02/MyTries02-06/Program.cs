013fdbaea55bb70783b1703090d34c7c2f7b4305
Hristo <hhristoff@yahoo.com>
2016-10-07 17:03:15 +0300

    Модул 02, Видео 08

--- Modul02/MyTries02-06/Program.cs
+++ Modul02/MyTries02-06/Program.cs
@@ -7,12 +7,26 @@ namespace MyTries0206
 		public static void Main (string[] args)
 		{
 		
-			String a = "Hello";
-			String b = "Word";
-			Object c = a + " + " + b;
+			int[] zada1 = new int [20];
+			zada1 [0] = 5*0;
+			zada1 [1] = 5*1;
+			zada1 [2] = 5*2;
+			zada1 [3] = 5*3;
+			zada1 [4] = 5*4;
+			zada1 [5] = 5*5;
+			zada1 [6] = 5*6;
+			zada1 [7] = 5*7;
 
+			Console.WriteLine ("елем на масива: " + zada1[0] + "," + zada1[1] + "," + zada1[2] + "," + zada1[3] + "," + zada1[4] + "," + zada1[5] + "," + zada1[6] + "\n\n");
 
-			Console.WriteLine (a + " + " + b);
+			string[] niki = "1,2,3,4,5,6,7,8,9".Split (',');
+			Console.WriteLine("Броят на масива:\n" + "1,2,3,4,5,6,7,8,9\n\ne: " + niki.Length + "\n\n");
+
+			string list1 = string.Join ("--", niki);
+			Console.WriteLine ("Новият стринг е:\n" + list1 + "\n\n");
+
+			string[] week = "M, T, W, T, F, S, S".Split ('-');
+			Console.WriteLine ("Дните от седмицата са:\n" + "M, T, W, T, F, S, S" +"\n\n");
 		}
 	}
 }
--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -6,19 +6,35 @@ namespace TableProject
 	{
 		public static void Main (string[] args)
 		{
-			string[] row = new string[3];
+			//Дефиниране таблица
+			string[,] table = new string[2, 3];
+				
+			//Въвеждане на първи ред
+			table[0, 0] = "Hristo"; table[0, 1] = "Hristov"; table[0, 2] = "31";
 
-			row [0] = "Ред 1";
-			row [1] = "Ред 2";
-			row [2] = "Ред 3";
+			//Въвеждане на втори ред
+			table[1, 0] = "Hristov"; table[1, 1] = "Hristo"; table[1, 2] = "41";
 
-			Console.WriteLine ("Масив row: " +  row [0] + ", " +  row [1] + ", " +  row [2] + "\n");
+			//Печат
+			Console.Write ("Кой ред искате да видите: ");
+			int _index = Convert.ToInt32 (Console.ReadLine ()) - 1;
 
-			string[] parse = "1,2,3,4,5,6,7,8,9".Split (',');
-			Console.WriteLine("Броят на масива:\n" + "1,2,3,4,5,6,7,8,9\n\ne: " + parse.Length);
+			Console.WriteLine ((table[_index,0])[0] + "." + table[_index,1] + ", " + table[_index,2] + "g.");
 
-			string list1 = string.Join (";", parse);
-			Console.WriteLine ("Новият стринг е:\n" + list1 + "\n\n");
 		}
 	}
 }
+
+//	string[] row = new string[3];
+
+//	row [0] = "Ред 1";
+//	row [1] = "Ред 2";
+//	row [2] = "Ред 3";
+
+//	Console.WriteLine ("Масив row: " +  row [0] + ", " +  row [1] + ", " +  row [2] + "\n");
+	
+//	string[] parse = "1,2,3,4,5,6,7,8,9".Split (',');
+//	Console.WriteLine("Броят на масива:\n" + "1,2,3,4,5,6,7,8,9\n\ne: " + parse.Length);
+
+//	string list1 = string.Join (";", parse);
+//	Console.WriteLine ("Новият стринг е:\n" + list1 + "\n\n");
