b46e1b0cedc3a779ccdd785acefba7d39e829e2a
Hristo <hhristoff@yahoo.com>
2016-10-18 20:30:55 +0300

    Модул 03, Видео 06 - Работа с условни оператори if

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -6,20 +6,35 @@ namespace BoolExplore
 	{
 		public static void Main (string[] args)
 		{
-			bool _b = true;	//true / false
+			int[] i = new int[] { 4, 3, 2 };
 
-			bool _test = 5 > 10;
+			int ui = Convert.ToInt32 ( Console.ReadLine ());
 
-			int[] _i = new int[] { 4, 3, 2 };
 
-			int _ui = Convert.ToInt32 ( Console.ReadLine ());
+//			bool check01 = ui <= i.Length;
+//			bool check02 = ui > 0;
+//
+//			bool exp01 = check01 && check02;	//Логическо и "§§"
+//			bool exp02 = check01 || check02;	//Логическо или "||"
+//			bool exp03 = !check01;			// Отрицание "!"
+//
+//			Console.WriteLine ("Логическо И:" + exp01.ToString ());
+//			Console.WriteLine ("Логическо ИЛИ:" + exp02.ToString ());
+//			Console.WriteLine ("Отрицание на " + check01.ToString () + ": " + exp03);
 
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
+			//Console.WriteLine (check01);
+			//Console.WriteLine (check02);
 
-			Console.WriteLine (_check01);
-			Console.WriteLine (_check02);
-			Console.WriteLine (_i [ _ui -1 ] );
-		}
+			if (ui > 0 && ui <= i.Length) {
+				Console.WriteLine (i [ui - 1]);
+			} else 
+				if (ui > i.Length) {
+					Console.WriteLine ("Индекса е прекалемо голям\n\n\n");
+				}
+				if (ui <= 0) {
+					Console.WriteLine ("Индекса е прекалемо малък\n\n\n");
+				}
+			}
 	}
 }
+
