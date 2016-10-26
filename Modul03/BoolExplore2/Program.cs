9b261658a2e41cd5a910e1f8813d81be4bf5d9f5
Hristo <hhristoff@yahoo.com>
2016-10-26 15:21:41 +0300

    Модул 04, Видео 05 - Цикъл do...while, while и for

--- Modul03/BoolExplore2/Program.cs
+++ Modul03/BoolExplore2/Program.cs
@@ -15,40 +15,54 @@ namespace BoolExplore2
 			input = Console.ReadLine ();
 			bool check01 = int.TryParse (input, out ui);
 
+			try
+			{
+				Console.WriteLine ("Елемент " + ui.ToString () + " от масива е: ");
+				Console.WriteLine (i [ui-1]);
+
+			}catch{
+				Console.WriteLine ("Недефиниран елемент");
+			}
+
+//			Използване на условно присвояване
+
 //			ui = (ui <= i.Length ) ? ui : 3;
 //			ui = (ui > 0) ? ui : 1;
 
-			switch (ui) {
-			case 1:
-				{
-					Console.WriteLine ("1-ви елемент от масива");
-					Console.WriteLine (i [ui-1]);
-					break;
-				}
-			
-			case 2:
-				{
-					Console.WriteLine ("1-ви елемент от масива");
-					Console.WriteLine (i [ui-1]);
-					break;
-				}
-			
-			case 3:
-				{
-					Console.WriteLine ("1-ви елемент от масива");
-					Console.WriteLine (i [ui-1]);
-					break;
+//			Използване на Switch
+//	
+//			switch (ui) {
+//			case 1:  
+//				{
+//					Console.WriteLine ("1-ви елемент от масива");
+//					Console.WriteLine (i [ui-1]);
+//					break;
+//				}
+//			
+//			case 2:
+//				{
+//					Console.WriteLine ("1-ви елемент от масива");
+//					Console.WriteLine (i [ui-1]);
+//					break;
+//				}
+//			
+//			case 3:
+//				{
+//					Console.WriteLine ("1-ви елемент от масива");
+//					Console.WriteLine (i [ui-1]);
+//					break;
+//				}
+//
+//			default:
+//				{
+//					Console.WriteLine ("В масива няма ст-ст за този индекс.\nОпитайте с индекс между 1 и 3.\n\n");
+//					break;
 				}
 
-			default:
-				{
-					Console.WriteLine ("В масива няма ст-ст за този индекс.\nОпитайте с индекс между 1 и 3.\n\n");
-					break;
-				}
+
 //			if (check01)
 //			{ Console.WriteLine (i [ui - 1]);
-		}
-
-		}
+//		}
+//		}
 	}
 }
